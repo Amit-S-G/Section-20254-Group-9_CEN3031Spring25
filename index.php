e069f177729d88fbebe3a255f72a9932db484b37
eric wang <erichwang2005@gmail.com>
2025-03-19 17:13:32 -0400

    Developed a demo landing page, can be further modified for final product

--- index.php
+++ index.php
@@ -10,7 +10,28 @@
     <link rel="stylesheet" href="styles/index.css">
 </head>
 <body>
+    <section class  = "mission">
+        <h1> Have Fun While Completing Tasks </h1>
+        <p> Complete tasks, earn rewards, and keep your pet happy! </p>
+        <button class="cta-button"> Get Started </button>
+    </section>
 
+    <section class = "features">
+        <div class = "feature">
+            <h2> Task Completion Rewards </h2>
+            <p> Earn coins, Buy Items every time you complete a task! </p>
+        </div>
+
+        <div class = "feature">
+            <h2> Interactive Pets </h2>
+            <p> Use your earned coins to take care of your virtual pet. </p>
+        </div>
+
+        <div class = "feature">
+            <h2> Play with Friends </h2>
+            <p> Add Friends, Compete who can make more progress! </p>
+        </div>
+    </section>
 </body>
 </html>
 
