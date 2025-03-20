99eefebb9bc1f19e85427f61d8470db657e55d9b
eric wang <erichwang2005@gmail.com>
2025-03-20 15:10:53 -0400

    Updated Login Page and made headers nicer

--- login.php
+++ login.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <link rel="stylesheet" href="styles/login.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
 </head>
 <body>
     <div class="container">
@@ -15,11 +16,15 @@
             <h2>Welcome to Taskemon</h2>
             <br><br>
             <!-- The required tags make the error message when one is not filled. Remove to replace with custom message from php script below -->
-            <label for="username">Username:</label>
-            <input type="text" id="username" name="username" required>
-            
-            <label for="password">Password:</label>
-            <input type="password" id="password" name="password" required>
+            <div class="input-container">
+                <i class="fas fa-user"></i>
+                <input type="text" id="username" name="username" placeholder="Username" required>
+            </div>
+
+            <div class="input-container">
+                <i class="fas fa-key"></i>
+                <input type="password" id="password" name="password" placeholder="Password" required>
+            </div>
             
             <input type="submit" name="login" value="Login">
         </form>
