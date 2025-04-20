0b61029f0c6abdc3233fe864514ab819dc70b072
eric wang <erichwang2005@gmail.com>
2025-04-20 02:48:59 -0400

    Edited about page to add more content

--- pages/profile.php
+++ pages/profile.php
@@ -35,7 +35,6 @@ $background_color = $profile['background_color'] ?? '#1a1a1a';
     <meta charset="UTF-8">
     <title>Your Profile</title>
     <link rel="stylesheet" href="../styles/profile.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
 </head>
 <body>
 
