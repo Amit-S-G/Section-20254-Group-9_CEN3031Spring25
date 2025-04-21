3ef15a1eb64c1ae1a5b71d4517983efa1460aac8
Amit "Sunny" Gupta <137424796+Amit-S-G@users.noreply.github.com>
2025-04-21 00:19:04 -0400

    Update friends.php

--- pages/friends.php
+++ pages/friends.php
@@ -65,7 +65,7 @@ $conn->close(); // Close DB connection
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="styles/login.css">
+    <link rel="stylesheet" href="styles/friends.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
 </head>
 
