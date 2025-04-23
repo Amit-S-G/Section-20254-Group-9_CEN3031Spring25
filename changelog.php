1c100221c2455f28f2458c5e18f74d8ad76d66e1
Tristan <147774910+TristanRatchev@users.noreply.github.com>
2025-04-22 23:29:33 -0400

    Changelog addition and features

---
+++ changelog.php
@@ -0,0 +1,24 @@
+<?php
+    include("header1.html")
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Change-Log</title>
+    <link rel="stylesheet" href="styles/changelog.css">
+</head>
+<body>
+    <header class="changelog-header">
+        <h1>Change-Log</h1>
+    </header>
+
+    <section class="contact-container">
+        <h2> ~ Change Timeline ~ </h2>
+        <p class ="get-in-touch">Our recent changes:</p>
+
+    </section>
+</body>
+</html>
